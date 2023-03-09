2d636a91314d807c91872acf59bc7261a9defc4c
Giles Knap <giles.knap@diamond.ac.uk>
2023-03-09 13:59:44 +0000

    add tests for hand coded substitution files

---
+++ tests/example-config/st.cmd
@@ -0,0 +1,13 @@
+# Example hand coded IOC startup script for example-config
+cd "/repos/epics/ioc"
+
+epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES", '6000000'
+epicsEnvSet "EPICS_CA_SERVER_PORT", '7064'
+
+dbLoadDatabase "dbd/ioc.dbd"
+ioc_registerRecordDeviceDriver(pdbbase)
+
+dbLoadRecords("config/calc.db")
+dbLoadRecords("/tmp/ioc.db")
+
+iocInit()
