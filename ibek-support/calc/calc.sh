fe82a5cde2acb54d134b869f62354a1aeec1af99
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-02 11:41:13 +0100

    restore submodule

--- ibek-support/calc/calc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# patch asyn for compatibility with the container base OS Ubuntu
-
-if [[ $TARGET_ARCHITECTURE == "rtems" ]]; then
-    # don't build the test directories (they don't compile on RTEMS)
-    sed -i /'DIRS +='/s/tests// Makefile
-fi
-
--- ibek-support/ioc/README.md
+++
@@ -1,5 +0,0 @@
-Test IOCs for ibek-support
-==========================
-
-This ioc folder is copied into the test images by Dockerfile.xxx in
-the tests folder.
